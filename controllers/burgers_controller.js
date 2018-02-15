59c84dd4df7e2f27874ef59a671cfa5a6abeb4a3
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2018-02-14 22:36:35 -0600

    Blind commit

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,16 +3,41 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var burgers = require("../models/burgers.js");
+var burger = require("../models/burger.js");
 
 //route the burgers
 router.get("/", function(req, res) {
-  burgers.all(function(data) {
+  burger.all(function(data) {
     var hbsObject = {
       burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.create([
+    "burger_name"
+  ], [
+    req.body.burger_name
+  ], function(result) {
+    console.log(req.body)
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
   });
 });
 
