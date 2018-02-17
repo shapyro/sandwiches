93b4700a552a52f4b4519c5c3aaa71a0d92926db
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2018-02-17 16:08:13 -0600

    Blind commit

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,15 @@
 var mysql = require("mysql"); 
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  port: 3306,
-  // password: "root",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnectio(nprocess.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      database: "burgers_db"
+  });
+}
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-
-  console.log("connected as id " + connection.threadId);
-});
-
+connection.connect();
 module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -25,12 +25,9 @@ function objToSql(ob) {
     var value = ob[key];
     // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(key + "=" + value);
     }
   }
@@ -40,7 +37,7 @@ function objToSql(ob) {
 }
 
 var orm = {
-  
+
   all: function(tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
     connection.query(queryString, function(err, result) {
